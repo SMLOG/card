312168e78eedf17f902dafc87a6359ff9af1b23c
alex <alex@10.82.145.130>
2023-08-18 17:54:18 +0800

    update

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #while ! git pull --rebase ; do echo 'lll';done;
+export NODE_OPTIONS=--openssl-legacy-provider
 
 (npm run build2 -- --dest='../cardwordswww' --no-clean )&&echo 'build done'
 (cd ../cardwordswww&&git add . && git commit -am 'update' && while ! git push ; do echo 'lll';done;)
